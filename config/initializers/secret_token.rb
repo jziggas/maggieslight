ad300e0c0ffe96df64d2315e19415487f04ac4f9
Joshua Ziggas <zoshjiggas@gmail.com>
2013-07-26 15:37:24 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Maggieslight::Application.config.secret_key_base = '399d297b6022e00bdadcbb2275139f117211ec08c699e0e4e5ab8a59dd6aa7ebb784d916eb106d4bb7c7c81942801f7ed0b4b743ca0cc5bbb0ebbfcfc856f632'
