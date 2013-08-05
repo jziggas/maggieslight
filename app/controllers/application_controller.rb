0ce92df3101616f7c652a8bd52210dbb48981402
Joshua Ziggas <zoshjiggas@gmail.com>
2013-08-05 17:11:44 -0400

    devise sanitizer override for User.name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_filter do
@@ -13,4 +16,11 @@ protect_from_forgery with: :exception
     redirect_to root_path, :alert => exception.message
   end
 
+
+  # Custom Devise sanitzer override for additional User model attributes ( i.e. User.name )
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
+  end
 end
