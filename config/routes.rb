b4da901b57534d1b0bd2e149b1f5a39327bdaa66
Joshua Ziggas <zoshjiggas@gmail.com>
2013-08-29 11:58:10 -0400

    always remember to RAILS_ENV=production when precompiling assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,22 @@
 Maggieslight::Application.routes.draw do
+
   devise_for :users
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
-  #resources :care_provider_profiles
 
-  #resources :care_receiver_profiles
-  resources 'those-who-care', as: :care_provider_profiles, controller: :care_provider_profiles do
+  resources 'those-who-care', as: :care_provider_profiles,
+    controller: :care_provider_profiles do
       member do
         get 'flag'
       end
     end
 
-  resources 'those-in-need', as: :care_receiver_profiles, controller: :care_receiver_profiles do
+  resources 'those-in-need', as: :care_receiver_profiles,
+    controller: :care_receiver_profiles do
       member do
         get 'flag'
       end
     end
-
-
 
   get "/profiles/", to: "user_profiles#index"
-  # get "/care-givers/", to: "care_provider_profiles#index"
-  # get "/care-receivers/", to: "care_receiver_profiles#index"
-  # /care-receivers/id
-  # /care-givers/id
-  # /care-receivers/id/edit
   root :to => "care_receiver_profiles#index"
-
-  # match "providers/:id/:action" => "care_provider_profiles#"
 end
