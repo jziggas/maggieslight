4b2586a3d82915f714b00f659c5e21eaa1c7be4a
Joshua Ziggas <zoshjiggas@gmail.com>
2013-09-09 00:38:46 -0400

    testing cancan seed yaml

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
-
+=begin
 puts 'ROLES'
 YAML.load(ENV['ROLES']).each do |role|
   Role.find_or_create_by_name(role)
@@ -17,6 +17,9 @@ puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
 user.add_role :admin
+=end
+Role.find_or_create_by_name("admin")
+Role.find_or_create_by_name("user")
 
 require 'csv'
 
