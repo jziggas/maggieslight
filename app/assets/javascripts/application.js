b38ad23a5e0939d85bcdee042c6c547affdb549b
Joshua Ziggas <zoshjiggas@gmail.com>
2013-09-12 17:31:41 -0400

    clickdesk code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,34 +17,6 @@
 //= require turbolinks
 //= require_tree .
 
-
-
-/*
-function initialize() {
-  //js code what you want run always
-  //var _glc =_glc || []; _glc.push('all_ag9zfmNsaWNrZGVza2NoYXRyDgsSBXVzZXJzGNig3BIM');
-  var glcpath = (('https:' == document.location.protocol) ? 'https://my.clickdesk.com/clickdesk-ui/browser/' :
-  'http://my.clickdesk.com/clickdesk-ui/browser/');
-  var glcp = (('https:' == document.location.protocol) ? 'https://' : 'http://');
-  var glcspt = document.createElement('script'); glcspt.type = 'text/javascript';
-  glcspt.async = true; glcspt.src = glcpath + 'livechat-new.js';
-  var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(glcspt, s);
-}
-*/
-//$(document).ready(initialize);
-//$(document).on('page:load', initialize);
-
-
-/* Raw Code
-  var _glc =_glc || []; _glc.push('all_ag9zfmNsaWNrZGVza2NoYXRyDgsSBXVzZXJzGNig3BIM');
-  var glcpath = (('https:' == document.location.protocol) ? 'https://my.clickdesk.com/clickdesk-ui/browser/' :
-  'http://my.clickdesk.com/clickdesk-ui/browser/');
-  var glcp = (('https:' == document.location.protocol) ? 'https://' : 'http://');
-  var glcspt = document.createElement('script'); glcspt.type = 'text/javascript';
-  glcspt.async = true; glcspt.src = glcpath + 'livechat-new.js';
-  var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(glcspt, s);
-*/
-
 $(document).ready(function() {
   $(".tree-toggle").on('click', function() {
     $(this).parent().children("ul.tree").toggle(200);
