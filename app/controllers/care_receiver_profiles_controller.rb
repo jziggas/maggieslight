12d80d39a17fb15e984c7c464439bff255562b74
Joshua Ziggas <zoshjiggas@gmail.com>
2014-03-04 12:57:19 -0500

    change three month survey rule to four weeks

--- app/controllers/care_receiver_profiles_controller.rb
+++ app/controllers/care_receiver_profiles_controller.rb
@@ -105,7 +105,7 @@ class CareReceiverProfilesController < ApplicationController
     def check_survey_status
       if user_signed_in?
         @employment_surveys = current_user.employment_surveys.sort_by(&:created_at)
-        if (@employment_surveys.empty? && current_user.created_at < 3.months.ago) || (@employment_surveys.last.created_at < 3.months.ago)
+        if (@employment_surveys.empty? && current_user.created_at < 4.weeks.ago) || (@employment_surveys.last.created_at < 4.weeks.ago)
           redirect_to new_employment_survey_path
         end
       end
