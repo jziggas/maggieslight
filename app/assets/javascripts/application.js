f252b64c62b4a31e9331c8e8135f02c8b344d269
Joshua Ziggas <zoshjiggas@gmail.com>
2014-03-30 03:44:56 -0400

    bootstrap 3 update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,35 +18,5 @@
 //= require_tree .
 
 $(document).ready(function() {
-  $(".tree-toggle").on('click', function() {
-    $(this).parent().children("ul.tree").toggle(200);
-  });
   $('.flag').tooltip();
-  // zopim_chat();
 });
-
-/* Place this in the application layout for zopim chat, inside .box01 under the body. Works with TurboLinks.
-
-    :javascript
-      function zopim_chat(){
-        $('[__jx__id], embed#__zopnetworkswf').remove();
-        window.$zopim = null;
-        (function(d,s){
-          var z = $zopim = function(c){
-            z._.push(c)
-          }, $ = z.s = d.createElement(s), e = d.body.getElementsByTagName(s)[0];
-          z.set = function(o){
-            z.set._.push(o)
-          };
-          z._ = [];
-          z.set._ = [];
-          $.async = !0;
-          $.setAttribute('charset','utf-8');
-          $.src = '//cdn.zopim.com/?<PUT YOUR API CODE HERE.. MINE IS IN LASTPASS>';
-          z.t = +new Date;
-          $.type = 'text/javascript';
-          e.parentNode.insertBefore($,e)
-        })(document,'script');
-      }
-
-*/
